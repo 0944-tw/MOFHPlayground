f112d71d8874db1986ee3dcc0c47deb96e278ab1
Loading.. Please Wait <69704016+ImLoadingUuU@users.noreply.github.com>
2022-11-17 17:33:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ Testing MOFH API Without creating MOFH Accounts!
  */
 
 const express = require('express');
+const port = 3000
 const router = express.Router()
 const app = express()
 const bodyParser = require('body-parser')
@@ -12,4 +13,5 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use('/', require('./account/create'))
 app.use('/', require('./domain/index'))
-app.listen(3000)
+console.log(`Listen on localhost:${port}. `)
+app.listen(port)
